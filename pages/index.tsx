15361f6ed7d3fdeee9a4273a5f6da5201d5985bc
CamiloVargas <camilovargasingm@gmail.com>
2022-04-13 18:57:48 -0500

    preparing interfaces

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,9 @@
-import type { NextPage } from 'next'
+import type { GetStaticProps, NextPage } from 'next'
+import { Product } from '../product/types';
+
+interface Props  {
+  products: Product[];
+}
 
 const Home: NextPage = () => {
   return (
@@ -6,6 +11,14 @@ const Home: NextPage = () => {
       Home
     </div>
   )
+}
+
+export const  getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {
+      products: []
+    }
+  }
 }
 
 export default Home
