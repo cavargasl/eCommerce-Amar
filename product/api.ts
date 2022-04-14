ac4277556c60b41a463de93f8a2ee4add4f27795
CamiloVargas <camilovargasingm@gmail.com>
2022-04-14 14:30:02 -0500

    corection price

--- product/api.ts
+++ product/api.ts
@@ -2,6 +2,10 @@ import axios from "axios";
 import Papa from "papaparse";
 import { Product } from "./types";
 
+const formatPrice = (price: number): number => {
+  return Number(price.toString().replace(/[^0-9.]+/g, ""))
+}
+
 const api = {
   list: async (): Promise<Product[]> => {
     return axios.get('https://docs.google.com/spreadsheets/d/e/2PACX-1vQLB-UfMt8pvtoABa6vo33KQxiL8LYdH2YWZF4IllaVMAVSNYjcslIRldqQ9PmV0d9b2z5aX5Gm5eO3/pub?gid=0&single=true&output=csv',
@@ -11,7 +15,13 @@ const api = {
       return new Promise<Product[]>((resolve, reject) => {
         Papa.parse(response.data, {
           header: true,
-          complete: results => resolve(results.data as Product[]),
+          complete: results => {
+            const products = results.data as Product[]
+            return resolve(products.map(product => ({
+              ...product,
+              price: formatPrice(product.price)
+            })))
+          },
           error: error =>  reject(error.message)
         })
       })
