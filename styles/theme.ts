ea3afe7ed7b12a5fcdf8b1dd30517be17a4bc915
CamiloVargas <camilovargasingm@gmail.com>
2022-04-24 01:43:24 -0500

    add deafult color

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,18 +1,18 @@
-import { extendTheme, theme } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
 
 export default extendTheme({
   colors: {
-    primary:{
-      50: "#EE73A0",
-      100: "#EE73A0",
-      200: "#EE73A0",
-      300: "#EE73A0",
+    primary: {
+      50: "#FFF5F7",
+      100: "#FED7E2",
+      200: "#FBB6CE",
+      300: "#F687B3",
       400: "#EE73A0",
-      500: "#EE73A0",
-      600: "#EE73A0",
-      700: "#EE73A0",
-      800: "#EE73A0",
-      900: "#EE73A0",
+      500: "#D53F8C",
+      600: "#B83280",
+      700: "#97266D",
+      800: "#702459",
+      900: "#521B41",
     }
   },
   semanticTokens: {
@@ -30,5 +30,14 @@ export default extendTheme({
         backgroundColor: "primary.50"
       }
     }
-  }
+  },
+  components: {
+    Button: {
+      variants: {
+        'with-shadow': {
+          boxShadow: 'transparent',
+        }
+      },
+    },
+  },
 })
